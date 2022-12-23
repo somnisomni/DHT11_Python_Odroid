01ffce5ed32db180ef36f2776413bd33b1ec91e3
somni <me@somni.one>
2022-12-23 17:29:42 +0900

    Update install requirement

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/somnisomni/DHT11_Python_Odroid",
     packages=find_packages(),
-    install_requires=["RPi.GPIO"],
+    install_requires=["odroid-wiringpi"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
