5c05453cb7b14bcb944fb4659d9dc9c8adee71b8
somni <me@somni.one>
2022-12-23 17:36:29 +0900

    Change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="dht11-odroid",
+    name="odroid-dht11",
     version="0.1.0",
     author="somni",
     author_email="me@somni.one",
